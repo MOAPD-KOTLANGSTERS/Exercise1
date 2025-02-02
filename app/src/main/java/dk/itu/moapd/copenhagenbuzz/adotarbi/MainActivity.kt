bdaff4fb0c9ece9f8a62f843419656cf24425f7a
André Birk <arbi@itu.dk>
2025-02-02 14:41:32 +0100

    removed unnecessary boilerplate code

--- app/src/main/java/dk/itu/moapd/copenhagenbuzz/adotarbi/MainActivity.kt
+++ app/src/main/java/dk/itu/moapd/copenhagenbuzz/adotarbi/MainActivity.kt
@@ -17,31 +17,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent {
-            Exercise1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
+        setContentView(R.layout.activity_main)
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Exercise1Theme {
-        Greeting("Android")
     }
 }
